39b542c870781d424f92396966d16850459e14cd
Andrew Gwynn <afx.studios.hq@gmail.com>
2024-12-16 20:28:04 -0800

    Initialize rs/updateCodeOwnersJan22

---
+++ ios-base/Extensions/FontExtension.swift
@@ -0,0 +1,48 @@
+//
+//  FontExtension.swift
+//  ios-base
+//
+//  Created by Germán Stábile on 2/28/19.
+//  Copyright © 2019 Rootstrap Inc. All rights reserved.
+//
+
+import UIKit
+import RSFontSizes
+
+extension UIFont {
+  static let h1Regular: UIFont = .font(size: .heading1).withWeight(.regular)
+  static let h2Regular: UIFont = .font(size: .heading2).withWeight(.regular)
+  static let h3Regular: UIFont = .font(size: .heading3).withWeight(.regular)
+  static let h1Medium: UIFont = .font(size: .heading1).withWeight(.medium)
+  static let h2Medium: UIFont = .font(size: .heading2).withWeight(.regular)
+  static let h3Medium: UIFont = .font(size: .heading3).withWeight(.regular)
+  
+  private func withWeight(_ weight: UIFont.Weight) -> UIFont {
+    var attributes = fontDescriptor.fontAttributes
+    var traits = (attributes[.traits] as? [UIFontDescriptor.TraitKey: Any]) ?? [:]
+    
+    traits[.weight] = weight
+    
+    attributes[.name] = nil
+    attributes[.traits] = traits
+    attributes[.family] = familyName
+    
+    let descriptor = UIFontDescriptor(fontAttributes: attributes)
+    
+    return UIFont(descriptor: descriptor, size: pointSize)
+  }
+  
+  static func font(withName name: String = "", size: Sizes) -> UIFont {
+    let size = Font.PointSize.proportional(to: (.screen6_5Inch,
+                                                size.rawValue)).value()
+    let font = UIFont(name: name,
+                      size: size)
+    return font ?? UIFont.systemFont(ofSize: size)
+  }
+
+  public enum Sizes: CGFloat {
+    case heading1 = 32.0
+    case heading2 = 16.0
+    case heading3 = 15.0
+  }
+}
